14aa943e22b4c9925b7f273c9e92537ceda03cfb
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-09-27 14:07:52 -0400

    boolean inclass

--- JohnsonHuang9/src/attendance/Student.java
+++ JohnsonHuang9/src/attendance/Student.java
@@ -3,6 +3,7 @@ package attendance;
 public class Student implements Attendee {
 	private String f;
 	private String l;
+	private boolean inClass = false;
 	public Student(String firstName,String lastName) {
 		this.f = firstName;
 		this.l = lastName; 
