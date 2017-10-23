c04d60537e8a7a8d95e0a86a5adf984528e5c219
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-10-23 15:30:53 -0400

    caveroom

---
+++ JohnsonHuang9/src/caveExplorer/CaveExplorer.java
@@ -0,0 +1,17 @@
+package caveExplorer;
+
+import java.util.Scanner;
+
+public class CaveExplorer {
+
+	public static CaveRoom[][] caves;
+	public static Scanner in;//for user input
+	public static CaveRoom currentRoom;//changes as the user moves
+	public static Inventory inventory;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
