b57e78b5dc3dbc778da4f91c6b2a5475f71dd237
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-11-21 14:24:54 -0500

    predator class

--- JohnsonHuang9/src/javaTheHardWay/Predator.java
+++ JohnsonHuang9/src/javaTheHardWay/Predator.java
@@ -1,14 +1,51 @@
 package javaTheHardWay;
 
-public class Predator extends Animal {
+public class Predator extends ReproductionAnimal {
 
-	public Predator() {
-		// TODO Auto-generated constructor stub
+	public Predator(Habitat matingArea) {
+		super(matingArea);
+		setMaxLitterSize(3);
 	}
-
+	
+	public Predator(Habitat matingArea, String description, Trait trait1, Trait trait2) {
+		super(matingArea, description, trait1, trait2);
+		setMaxLitterSize(3);
+	}
+	
+	public static final String[] PREDATORS = {"wolf","bear","lion", "bobcat"};
+	
+	public String getName() {
+		return PREDATORS[(int)(Math.random()* PREDATORS.length)];
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Wilderness wilderness = new Wilderness("A Wooded Area", 10);
+		Predator p = new Predator(wilderness);
+		wilderness.addAnimal(p);
+		wilderness.simulate(2);
 	}
-
+	
+	public ReproductionAnimal getOffspring(ReproductionAnimal mate) {
+		return new Predator(habitat, getDescription(),
+				Trait.getDominantTrait(getTrait1(), mate.getTrait1()),
+				Trait.getDominantTrait(getTrait2(), mate.getTrait2()));
+	}
+	
+	public boolean canEat() {
+		int attempts = 0;
+		while(attempts < 3) {
+			int tIndex = (int)(Math.random()* habitat.getAnimals().length);
+			Animal target = habitat.getAnimals()[tIndex];
+			if(target instanceof Prey) {
+				habitat.removeAnimal(tIndex);
+				return true;
+			}
+			attempts++;
+		}
+		return false;
+	}
+	//1.It returns a predator using the constructor
+	//2. There is a 50 50 chance of the predator eating a prey. If there is more
+	//prey, there is a higher chance of the predator being able to eat, and if there is less
+	//there is a lower chance
 }
